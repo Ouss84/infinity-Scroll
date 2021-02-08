4da480b75fcbb9f51ae80b8282da094134f53c33
Ouss Bahri <bahrioussama84@gmail.com>
2021-02-08 21:32:14 +0200

    intitializing the getRandomPicture function

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 const apiKey = 'y_ZigdkU21C4L0jQAKpueAkX7DnJG4mDn3wKYSztr60';
 const count = 10;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// get random pictures function
+async function getRandomPicture (){
+    try{
+        const response = await fetch (apiUrl);
+        const data = await response.json();
+        console.log(data);
+    }catch(error){
+        
+    }
+}
+
+//run the app
+getRandomPicture()
