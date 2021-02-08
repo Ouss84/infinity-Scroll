537ca70763153967cd38592fe5fcb1abf5db1532
Ouss Bahri <bahrioussama84@gmail.com>
2021-02-08 21:17:14 +0200

    setting up the apiUrl and the apiKey in Unsplash

---
+++ script.js
@@ -0,0 +1,4 @@
+// Unsplash API
+const apiKey = 'y_ZigdkU21C4L0jQAKpueAkX7DnJG4mDn3wKYSztr60';
+const count = 10;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
