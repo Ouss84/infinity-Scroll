d99570fa5593414ab00a0f019ee8080acbefbf81
Ouss Bahri <bahrioussama84@gmail.com>
2021-02-10 19:44:46 +0200

    adding the load to display function to check the loaded images

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const imageConatainer = document.getElementById ('image-container');
 const loader = document.getElementById ('laoder');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 
 // Unsplash API
 const apiKey = 'y_ZigdkU21C4L0jQAKpueAkX7DnJG4mDn3wKYSztr60';
-const count = 10;
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const count = 30;
+// const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// check if all images were loaded
+function loadedImages(){
+    imagesLoaded++;
+    if(imagesLoaded === totalImages){
+        ready = true;
+        console.log("image loaded", imageLoaded)
+        console.log("ready", ready)
+    }
+}
 
 // setAttribute function to dry
 function setAttributes (element, attributes){
@@ -18,6 +31,8 @@ function setAttributes (element, attributes){
 
 // creating the function that displays photos
 function displayPhotos(){
+    totalImages = photosArray.length;
+    console.log ("total images",totalImages);
     //run a forEach loop to the photosArray
     photosArray.forEach ((photo) => {
         //creating the anchor element
@@ -30,12 +45,14 @@ function displayPhotos(){
         //creating the image inside the anchor
         const image = document.createElement ('img');
         //defining the image attributes
-        
         setAttributes(image, {
             src : photo.urls.regular,
             alt : photo.alt_description,
             title : photo.alt_description,
         });
+        // add eventlistener to check when images are loaded
+        image.addEventListener('load', loadedImages);
+
         // appending the image inside the anchor and then the anchor inside the image-container
         anchor.appendChild(image);
         imageConatainer.appendChild(anchor);
@@ -54,9 +71,9 @@ async function getRandomPicture (){
 
 //implementing the load functionality
 window.addEventListener('scroll', () =>{
-    if ( window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
+    if ( window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
         getRandomPicture();
-        console.log('loaded')
     }
 })
 
